d8fb6bb74ed60d855f2ef6788fcee297b9dc6c94
NatanaelBraga-dev <natanaelcordeirobraga@gmail.com>
2026-05-25 17:00:04 -0300

    adicionando carrossel de parceiros e ajustando footer

--- src/components/partners_section/PartnersSection.jsx
+++ src/components/partners_section/PartnersSection.jsx
@@ -2,18 +2,66 @@ import './PartnersSection.css';
 
 function PartnersSection() {
 
+    const partners = [
+        {
+            name: 'BMW',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/4/44/BMW.svg'
+        },
+        {
+            name: 'HYUNDAI',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/4/44/Hyundai_Motor_Company_logo.svg'
+        },
+        {
+            name: 'TOYOTA',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Toyota_carlogo.svg'
+        },
+        {
+            name: 'JEEP',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/9/90/Jeep_logo.svg'
+        },
+        {
+            name: 'AUDI',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/6/6f/Audi_logo_detail.svg'
+        },
+        {
+            name: 'MERCEDES',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/9/90/Mercedes-Logo.svg'
+        }
+    ];
+
+    const infinitePartners = [
+        ...partners,
+        ...partners,
+        ...partners,
+        ...partners
+    ];
+
     return (
 
         <section className="partners-section">
 
-            <h2>Principais parceiros</h2>
+            <h2>NOSSOS PARCEIROS</h2>
 
-            <div className="flex justify-content-center align-items-center flex-wrap gap-6">
+            <div className="carousel">
 
-                <div className="partner-logo">BMW</div>
-                <div className="partner-logo">HYUNDAI</div>
-                <div className="partner-logo">TOYOTA</div>
-                <div className="partner-logo">JEEP</div>
+                <div className="carousel-track">
+
+                    {infinitePartners.map((partner, index) => (
+
+                        <div className="partner-card" key={index}>
+
+                            <img
+                                src={partner.logo}
+                                alt={partner.name}
+                            />
+
+                            <h3>{partner.name}</h3>
+
+                        </div>
+
+                    ))}
+
+                </div>
 
             </div>
 
